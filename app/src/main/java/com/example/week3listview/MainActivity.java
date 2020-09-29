461974cd9b1329c3a9cb84ea14430b43f07623d0
rania <raniaarbash@hotmail.com>
2020-09-29 11:52:16 -0400

    Add a layout file for spinner row.

--- app/src/main/java/com/example/week3listview/MainActivity.java
+++ app/src/main/java/com/example/week3listview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.week3listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,28 +16,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    String collegeList[] = {};
-    String selectedColleges[] = {};
+    String collegeList[] = {};// source for spinner
+    String selectedColleges[] = {};// source for list view
+
     Spinner collegeSpinner;
     Button add;
+
     int index = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         collegeSpinner = (Spinner)findViewById(R.id.spinner);
         add = (Button)findViewById(R.id.add);
-        selectedColleges = new String[10];
-        Arrays.fill(selectedColleges, "");
-
         ListView listview = (ListView) findViewById(R.id.simpleListView);
+
+
+
+
+
         collegeList = new String[]{"Seneca", "Humber", "George Brown", "Centennial", "Niagra", "Sheirdan", "Durham"};
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view_item, R.id.textview, selectedColleges);
-        listview.setAdapter(arrayAdapter);
+        selectedColleges = new String[10];
 
-        ArrayAdapter<String> collegeAdapter = new ArrayAdapter<String>(this, R.layout.list_view_item, R.id.textview, collegeList);
-        collegeSpinner.setAdapter(collegeAdapter);
+        Arrays.fill(selectedColleges, "");
+
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this,R.layout.spinner_row_desing,R.id.collegeTextID,collegeList);
+        collegeSpinner.setAdapter(spinnerArrayAdapter);
+
+
+        ArrayAdapter<String> listArrayAdapter = new ArrayAdapter<>(this,R.layout.list_view_item,R.id.textview,selectedColleges);
+        listview.setAdapter(listArrayAdapter);
+
+
+
         listview.setOnItemClickListener(messageClickedHandler);
 
         add.setOnClickListener(new View.OnClickListener() {
@@ -45,18 +62,23 @@ public class MainActivity extends AppCompatActivity {
                final String selectedCollege =  collegeSpinner.getSelectedItem().toString();
 
                 boolean contains = false;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     contains = Arrays.stream(selectedColleges).anyMatch(s -> {return s.equals(selectedCollege);});
                 }
                 System.out.println(contains);
 
 
-
+                //convert array into list then check contains
                 if(!Arrays.asList(selectedColleges).contains(selectedCollege)) {
                   selectedColleges[index] = selectedCollege;
+
                   Toast.makeText(getApplicationContext(), selectedCollege + "College Inserted", Toast.LENGTH_SHORT).show();
-                  arrayAdapter.notifyDataSetChanged();
+
+                  listArrayAdapter.notifyDataSetChanged();
                   index++;
+
               }else {
                   Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
               }
@@ -66,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView parent, View v, int position, long id) {
+
             String value = (String)parent.getItemAtPosition(position);
             Toast toast=Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT);
             toast.show();
